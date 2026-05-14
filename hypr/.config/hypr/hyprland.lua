c733267ed6cacc2f4fd2390e137d53999a89eef4
Terrapin <freq.modulada@gmail.com>
2026-05-14 16:45:54 -0400

    put enviroment variables in its own file

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -4,16 +4,7 @@
 require("monitors")
 require("programs")
 require("autostart")
-
--------------------------------
----- ENVIRONMENT VARIABLES ----
--------------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-
+require("env")
 
 -----------------------
 ----- PERMISSIONS -----
