7d1ae03f3b5d9905f76194a2047bba1d2b21502f
Terrapin <freq.modulada@gmail.com>
2026-05-14 16:33:29 -0400

    put autostart into its own file

---
+++ hypr/.config/hypr/autostart.lua
@@ -0,0 +1,11 @@
+-------------------
+---- AUTOSTART ----
+-------------------
+
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+hl.on("hyprland.start", function () 
+  hl.exec_cmd("waybar & hyprpaper")
+end)
+
+
--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -3,22 +3,7 @@
 
 require("monitors")
 require("programs")
-
--------------------
----- AUTOSTART ----
--------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Autostart/
-
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function () 
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
-
+require("autostart")
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
