8d797ec1279db4da22168455ba721706dfe5e9bd
Terrapin <freq.modulada@gmail.com>
2026-05-14 16:19:14 -0400

    set monitors for desktop and laptop

--- hypr/.config/hypr/monitors.lua
+++ hypr/.config/hypr/monitors.lua
@@ -3,6 +3,10 @@
 ------------------
 
 -- See https://wiki.hypr.land/Configuring/Basics/Monitors/
+
+local p = require("profiles")
+
+-- fallback
 hl.monitor({
     output   = "",
     mode     = "preferred",
@@ -10,4 +14,25 @@ hl.monitor({
     scale    = "auto",
 })
 
+if p.host_type == "desktop" then
+	hl.monitor({
+		output = p.main_monitor,
+		mode = "1920x1080@144",
+		position = "0x0",
+		scale = 1,
+	})
 
+	hl.monitor({
+		output = p.secondary_monitor,
+		mode = "1280x1024",
+		position = "-1280x0",
+		scale = 1,
+	})
+else
+	hl.monitor({
+		output = p.main_monitor,
+		mode = "1920x1080@144",
+		position = "0x0",
+		scale = 1.20,
+	})
+end
