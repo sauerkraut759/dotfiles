bd7acd1d8a584fa97abcdf119302fdc79ba11816
Terrapin <freq.modulada@gmail.com>
2026-05-14 16:56:46 -0400

    put general config into its own file

---
+++ hypr/.config/hypr/general.lua
@@ -0,0 +1,19 @@
+hl.config({
+	general = {
+		gaps_in  = 5,
+		gaps_out = 10,
+		border_size = 2,
+
+		col = {
+			active_border	= { colors = {"rgba(aee2eaee)", "rgba(81a1c1ee)"}, angle = 20 },
+			inactive_border = "rgba(808080aa)",
+		},
+
+		resize_on_border = false,
+
+		-- https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/
+		allow_tearing = true,
+
+		layout = "dwindle",
+	},
+})
--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -5,6 +5,7 @@ require("monitors")
 require("programs")
 require("autostart")
 require("env")
+require("general")
 
 -----------------------
 ----- PERMISSIONS -----
@@ -31,25 +32,7 @@ require("env")
 
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
-    general = {
-        gaps_in  = 5,
-        gaps_out = 20,
 
-        border_size = 2,
-
-        col = {
-            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
-            inactive_border = "rgba(595959aa)",
-        },
-
-        -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-        resize_on_border = false,
-
-        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-        allow_tearing = false,
-
-        layout = "dwindle",
-    },
 
     decoration = {
         rounding       = 10,
