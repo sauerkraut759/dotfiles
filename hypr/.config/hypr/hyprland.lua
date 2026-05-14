f5e7eaa6e94b261b51422752bb8fe0a664da57bc
Terrapin <freq.modulada@gmail.com>
2026-05-14 16:05:42 -0400

    moved monitor config to its own file

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -1,26 +1,5 @@
--- This is an example Hyprland Lua config file.
 -- Refer to the wiki for more information.
 -- https://wiki.hypr.land/Configuring/Start/
-
--- Please note not all available settings / options are set here.
--- For a full list, see the wiki
-
--- You can (and should!!) split this configuration into multiple files
--- Create your files separately and then require them like this:
--- require("myColors")
-
-
-------------------
----- MONITORS ----
-------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Monitors/
-hl.monitor({
-    output   = "",
-    mode     = "preferred",
-    position = "auto",
-    scale    = "auto",
-})
 
 
 ---------------------
