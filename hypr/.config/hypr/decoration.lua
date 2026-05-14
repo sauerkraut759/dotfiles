f15886faaa7848d6988a65b39ffb1b4a0728397b
Terrapin <freq.modulada@gmail.com>
2026-05-14 17:06:40 -0400

    put decoration into its own file

---
+++ hypr/.config/hypr/decoration.lua
@@ -0,0 +1,14 @@
+hl.config({
+	decoration = {
+		active_opacity	 = 1.0,
+		inactive_opacity = 0.8,
+
+		blur = {
+			enabled   = true,
+			size	  = 5,
+			passes	  = 2,
+			vibrancy  = 0.1696,
+			noise = 0.02,
+		},
+	},
+})
--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -6,6 +6,7 @@ require("programs")
 require("autostart")
 require("env")
 require("general")
+require("decoration")
 
 -----------------------
 ----- PERMISSIONS -----
@@ -34,28 +35,7 @@ require("general")
 hl.config({
 
 
-    decoration = {
-        rounding       = 10,
-        rounding_power = 2,
 
-        -- Change transparency of focused and unfocused windows
-        active_opacity   = 1.0,
-        inactive_opacity = 1.0,
-
-        shadow = {
-            enabled      = true,
-            range        = 4,
-            render_power = 3,
-            color        = 0xee1a1a1a,
-        },
-
-        blur = {
-            enabled   = true,
-            size      = 3,
-            passes    = 1,
-            vibrancy  = 0.1696,
-        },
-    },
 
     animations = {
         enabled = true,
