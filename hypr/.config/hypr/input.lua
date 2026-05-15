858cc397e1040fcf53e32f9a2af4b573c66301fb
Terrapin <freq.modulada@gmail.com>
2026-05-15 10:27:45 -0400

    put input config into its own file

---
+++ hypr/.config/hypr/input.lua
@@ -0,0 +1,34 @@
+---------------
+---- INPUT ----
+---------------
+
+hl.config({
+    input = {
+        kb_layout  = "us,latam",
+        kb_variant = "",
+        kb_model   = "",
+        kb_options = "lv3:ralt_alt",
+        kb_rules   = "",
+
+        follow_mouse = 2,
+
+	accel_profile = "flat",
+        sensitivity = 0, -- -1.0 - 1.0, 0 means no modification.
+
+        touchpad = {
+            natural_scroll = false,
+        },
+    },
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace",
+})
+
+hl.gesture({
+	fingers = 3,
+	direction = "vertical",
+	action = "special",
+})
