d8deadee85df6c34792c71805ca5fd95f67e10e3
Terrapin <freq.modulada@gmail.com>
2026-05-15 10:05:28 -0400

    put layout configs into its own file

---
+++ hypr/.config/hypr/layouts.lua
@@ -0,0 +1,20 @@
+-- https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/
+hl.config({
+    dwindle = {
+        preserve_split = true, -- You probably want this
+    },
+})
+
+-- https://wiki.hypr.land/Configuring/Layouts/Master-Layout/
+hl.config({
+    master = {
+        new_status = "master",
+    },
+})
+
+-- https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/
+hl.config({
+    scrolling = {
+        fullscreen_on_one_column = true,
+    },
+})
