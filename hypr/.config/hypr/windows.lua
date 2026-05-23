a8af21d782a489f36de168a1ab19ba6eca005abc
Terrapin <freq.modulada@gmail.com>
2026-05-23 13:04:37 -0400

    added missing windowrules

--- hypr/.config/hypr/windows.lua
+++ hypr/.config/hypr/windows.lua
@@ -37,3 +37,45 @@ hl.window_rule({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+-- windowrule = match:class kitty, opacity 1.0 override
+-- windowrule = match:title .*(mpv).*, float on
+-- windowrule = match:title .*(mpv).*, opacity 1.0 override
+-- windowrule = match:title .*(YouTube).*, opacity 1.0 override
+-- windowrule = match:class steam, float on
+-- windowrule = match:class steam, match:title Steam, tile on, size 80% 100%, workspace 2
+-- windowrule = match:class steam, match:title .*(Friends List).*, tile on, size 20% 100%, workspace 2
+-- windowrule = match:class org.pwmt.zathura, opacity 1.0 override
+-- windowrule = match:class swayimg, float on
+-- windowrule = match:class PacketTracer, float on
+
+hl.window_rule({
+	name = "kitty-rules",
+	match = { class = "kitty" },
+	opacity = "1.0 override",
+})
+
+hl.window_rule({
+	name = "mpv-rules",
+	match = { title = ".*(mpv).*" },
+	opacity = "1.0 override",
+	float = true,
+})
+
+hl.window_rule({
+	name = "youtube-rules",
+	match = { title = ".*(YouTube).*" },
+	opacity = "1.0 override",
+})
+
+hl.window_rule({
+	name = "zathura-rules",
+	match = { class = "org.pwmt.zathura" },
+	opacity = "1.0 override",
+})
+
+hl.window_rule({
+	name = "swayimg-rules",
+	match = { class = "swayimg" },
+	float = true,
+})
