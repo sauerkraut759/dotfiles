7c7b3c2826c6479b3f3bb5f45ac49a061539a3eb
Terrapin <freq.modulada@gmail.com>
2026-05-24 13:19:55 -0400

    attempt to fix persistent workspaces

--- hypr/.config/hypr/workspaces.lua
+++ hypr/.config/hypr/workspaces.lua
@@ -7,8 +7,13 @@
 local profile = require("profiles")
 
 -- Persistent monitors
-hl.workspace_rule({ workspace = "r[1-4]", monitor = profile.main_monitor, persistent = true })
 
 if profile.secondary_monitor then
-	hl.workspace_rule({ workspace = "r[5-8]", monitor = profile.secondary_monitor, persistent = true })
+	for i = 5,8 do
+		hl.workspace_rule({ workspace = tostring(i), monitor = profile.secondary_monitor, persistent = true })
+	end
+end
+
+for i = 1,4 do
+	hl.workspace_rule({ workspace = tostring(i), monitor = profile.main_monitor, persistent = true })
 end
